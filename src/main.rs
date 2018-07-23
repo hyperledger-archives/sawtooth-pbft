bridger-herman/sawtooth-pbft-old

Begin framework for PBFT consensus
Add the necessary dependencies, fill in the main entry point, and start
a skeleton of the protobuf messages.

Signed-off-by: Bridger Herman <bridger@bitwise.io>

src/main.rs
@@ -1,7 +1,84 @@
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
+
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate log4rs;
 extern crate sawtooth_sdk;
 
-use sawtooth_sdk::consensus::{engine::*, service::Service};
+mod engine;
+
+use std::process;
+
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
+use engine::PbftEngine;
+use sawtooth_sdk::consensus::{driver::Driver, zmq_driver::ZmqDriver};
 
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(pbft =>
+        (version: crate_version!())
+        (about: "PBFT Consensus Engine (Rust)")
+        (@arg connect: -C --connect +takes_value
+         "connection endpoint for validator")
+        (@arg verbose: -v --verbose +multiple
+         "increase output verbosity"))
+        .get_matches();
+
+    let endpoint = matches
+        .value_of("connect")
+        .unwrap_or("tcp://localhost:5005");
+
+    let console_log_level;
+    match matches.occurrences_of("verbose") {
+        0 => console_log_level = LevelFilter::Warn,
+        1 => console_log_level = LevelFilter::Info,
+        2 => console_log_level = LevelFilter::Debug,
+        3 | _ => console_log_level = LevelFilter::Trace,
+    }
+
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{h({l:5.5})} | {({M}:{L}):20.20} | {m}{n}",
+        )))
+        .build();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(console_log_level))
+        .unwrap_or_else(|err| {
+            error!("{}", err);
+            process::exit(1);
+        });
+
+    log4rs::init_config(config).unwrap_or_else(|err| {
+        error!("{}", err);
+        process::exit(1);
+    });
+
+    let driver = ZmqDriver::new(Box::new(PbftEngine::new()));
+
+    driver.start(&endpoint).unwrap_or_else(|err| {
+        error!("{}", err);
+        process::exit(1);
+    });
 }

hyperledger-archives/sawtooth-pbft

src/main.rs
@@ -1,7 +1,72 @@
-extern crate sawtooth_sdk;
+/*
+ * Copyright 2018 Bitwise IO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -----------------------------------------------------------------------------
+ */
 
-use sawtooth_sdk::consensus::{engine::*, service::Service};
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate protobuf;
+extern crate sawtooth_sdk;
+extern crate simple_logger;
+
+use std::process;
+
+use sawtooth_sdk::consensus::zmq_driver::ZmqDriver;
+
+mod engine;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(sawtooth_pbft =>
+        (version: crate_version!())
+        (about: "PBFT consensus for Sawtooth")
+        (@arg connect: -C --connect +takes_value
+         "connection endpoint for validator")
+        (@arg verbose: -v --verbose +multiple
+         "increase output verbosity")
+        (@arg ID: +required "the PBFT node's id"))
+        .get_matches();
+
+    let log_level = match matches.occurrences_of("verbose") {
+        0 =>  log::Level::Warn,
+        1 =>  log::Level::Info,
+        2 =>  log::Level::Debug,
+        3 | _ =>  log::Level::Trace,
+    };
+
+    let endpoint = String::from(
+        matches
+        .value_of("connect")
+        .unwrap_or("tcp://localhost:5050")
+    );
+
+    let id = value_t!(matches.value_of("ID"), u64)
+        .unwrap_or_else(|e| e.exit());
+
+    simple_logger::init_with_level(log_level).unwrap();
+
+    info!("Sawtooth PBFT Engine ({})", env!("CARGO_PKG_VERSION"));
+
+    let raft_engine = engine::PbftEngine::new(id);
+
+    let (driver, _stop) = ZmqDriver::new();
+
+    info!("PBFT Node {} connecting to '{}'", &id, &endpoint);
+    driver.start(&endpoint, raft_engine).unwrap_or_else(|err| {
+        error!("{}", err);
+        process::exit(1);
+    });
 }