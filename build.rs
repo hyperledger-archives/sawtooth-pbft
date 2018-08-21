adbfa66515eb5ba18f13d415d9606934769bbbb6
Bridger Herman <bridger@bitwise.io>
2018-08-21 08:47:22 -0500

    Redirect Protobuf-generated code
    The old method of generating protobuf code inside the src directory is
    no longer supported in Rust 1.28:
    https://blog.rust-lang.org/2018/08/02/Rust-1.28.html
    
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

--- build.rs
+++ build.rs
@@ -19,24 +19,29 @@ extern crate protoc_rust;
 
 use protoc_rust::Customize;
 
+use std::env;
 use std::fs;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
-    fs::create_dir_all("src/protos").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("protos");
+    let proto_path = Path::new("./protos");
+    fs::create_dir_all(&dest_path).unwrap();
 
     // Run protoc
     protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/protos",
-        input: &["protos/pbft_message.proto"],
-        includes: &["protos"],
+        out_dir: &dest_path.to_str().unwrap(),
+        input: &[proto_path.join("pbft_message.proto").to_str().unwrap()],
+        includes: &[proto_path.to_str().unwrap()],
         customize: Customize {
             ..Default::default()
         },
     }).expect("Protoc Error");
 
     // Create mod.rs accordingly
-    let mut mod_file = File::create("src/protos/mod.rs").unwrap();
+    let mut mod_file = File::create(dest_path.join("mod.rs")).unwrap();
     mod_file.write_all(b"pub mod pbft_message;\n").unwrap();
 }
