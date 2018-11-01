5adc5ea418f05caf538ae0e7bf3c3b9f2d766862
Adam Ludvik <ludvik@bitwise.io>
2018-11-01 16:29:59 -0700

    Add single-node crash fault test (not enabled)
    Add an integration test that the network can handle and recover from a single
    node failure.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- tests/sawtooth-pbft-test.dockerfile
+++ tests/sawtooth-pbft-test.dockerfile
@@ -21,6 +21,7 @@ RUN echo "deb [arch=amd64] http://repo.sawtooth.me/ubuntu/bumper/stable xenial u
  || apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 8AA7AF1F1091A5FD) \
  && apt-get update \
  && apt-get install -y -q --allow-downgrades \
+    apt-transport-https \
     build-essential \
     curl \
     libssl-dev \
@@ -29,15 +30,30 @@ RUN echo "deb [arch=amd64] http://repo.sawtooth.me/ubuntu/bumper/stable xenial u
     pkg-config \
     python3 \
     python3-sawtooth-cli \
+    python3-sawtooth-intkey \
     python3-sawtooth-rest-api \
     python3-sawtooth-settings \
     python3-sawtooth-validator \
     python3-requests \
     python3-nose2 \
     sawtooth-smallbank-workload \
+    sawtooth-intkey-workload \
     sawtooth-smallbank-tp-go \
     sawtooth-xo-tp-go \
+    software-properties-common \
     unzip \
+ # Install docker
+ && curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey \
+ && apt-key add /tmp/dkey \
+ && add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo \"$ID\") \
+   $(lsb_release -cs) \
+   stable" \
+ && apt-get update \
+ && apt-get -y install docker-ce \
+ # Install docker-compose
+ && curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+ && chmod +x /usr/local/bin/docker-compose \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/*
 
---
+++ tests/test_crash_fault_tolerance.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#
+# Copyright 2018 Intel Corporation
+# Copyright 2018 Cargill Incorporated
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script tests dynamic membership by starting a PBFT network with 4
+# nodes, applying a workload, checking that all 4 nodes reach block 10, killing
+# a node, verifying all nodes reach block 20, restarting the node, and
+# verifying that all nodes reach block 30.
+
+if [ -z "$ISOLATION_ID " ]; then export ISOLATION_ID=latest; fi
+
+echo "Starting initial network"
+docker-compose -p ${ISOLATION_ID} -f /project/sawtooth-pbft/adhoc/admin.yaml up -d
+docker-compose -p ${ISOLATION_ID}-alpha -f /project/sawtooth-pbft/adhoc/node.yaml up -d
+docker-compose -p ${ISOLATION_ID}-beta -f /project/sawtooth-pbft/adhoc/node.yaml up -d
+docker-compose -p ${ISOLATION_ID}-gamma -f /project/sawtooth-pbft/adhoc/node.yaml up -d
+GENESIS=1 docker-compose -p ${ISOLATION_ID}-epsilon -f /project/sawtooth-pbft/adhoc/node.yaml up -d
+
+ADMIN=${ISOLATION_ID}_admin_1
+
+echo "Gathering list of initial keys and REST APIs"
+INIT_KEYS=($(docker exec $ADMIN bash -c '\
+  cd /shared_data/validators && paste $(ls -1) -d , | sed s/,/\ /g'))
+echo "Initial keys:" ${INIT_KEYS[*]}
+INIT_APIS=($(docker exec $ADMIN bash -c 'cd /shared_data/rest_apis && ls -d *'))
+echo "Initial APIs:" ${INIT_APIS[*]}
+
+echo "Waiting until network has started"
+docker exec -e API=${INIT_APIS[0]} $ADMIN bash -c 'while true; do \
+  BLOCK_LIST=$(sawtooth block list --url "http://$API:8008" 2>&1); \
+  if [[ $BLOCK_LIST == *"BLOCK_ID"* ]]; then \
+    echo "Network ready" && break; \
+  else \
+    echo "Still waiting..." && sleep 0.5; \
+  fi; done;'
+
+echo "Starting workload"
+RATE=1 docker-compose -p ${ISOLATION_ID} -f adhoc/workload.yaml up -d
+
+echo "Waiting for all nodes to reach block 10"
+docker exec $ADMIN bash -c '\
+  APIS=$(cd /shared_data/rest_apis && ls -d *); \
+  NODES_ON_10=0; \
+  until [ "$NODES_ON_10" -eq 4 ]; do \
+    NODES_ON_10=0; \
+    sleep 5; \
+    for api in $APIS; do \
+      BLOCK_LIST=$(sawtooth block list --url "http://$api:8008" \
+        | cut -f 1 -d " "); \
+      echo $api && echo $BLOCK_LIST;
+      if [[ $BLOCK_LIST == *"10"* ]]; then \
+        echo "API $api is on block 10" && ((NODES_ON_10++)); \
+      else \
+        echo "API $api is not yet on block 10"; \
+      fi; \
+    done; \
+  done;'
+echo "All nodes have reached block 10!"
+
+echo "Shutting down alpha node"
+docker-compose -p ${ISOLATION_ID}-alpha -f adhoc/node.yaml stop pbft
+
+echo "Waiting for remaining nodes to reach block 20"
+docker exec $ADMIN bash -c '\
+  APIS=$(cd /shared_data/rest_apis && ls -d *); \
+  NODES_ON_20=0; \
+  until [ "$NODES_ON_20" -gt 2 ]; do \
+    NODES_ON_20=0; \
+    sleep 5; \
+    for api in $APIS; do \
+      BLOCK_LIST=$(sawtooth block list --url "http://$api:8008" 2> /dev/null \
+        | cut -f 1 -d " "); \
+      echo $api && echo $BLOCK_LIST;
+      if [[ $BLOCK_LIST == *"20"* ]]; then \
+        echo "API $api is on block 20" && ((NODES_ON_20++)); \
+      else \
+        echo "API $api is not yet on block 20"; \
+      fi; \
+    done; \
+  done;'
+echo "All nodes have reached block 20!"
+
+echo "Restarting alpha node"
+docker-compose -p ${ISOLATION_ID}-alpha -f adhoc/node.yaml start pbft
+
+echo "Waiting for all nodes to reach block 30"
+docker exec $ADMIN bash -c '\
+  APIS=$(cd /shared_data/rest_apis && ls -d *); \
+  NODES_ON_30=0; \
+  until [ "$NODES_ON_30" -eq 4 ]; do \
+    NODES_ON_30=0; \
+    sleep 5; \
+    for api in $APIS; do \
+      BLOCK_LIST=$(sawtooth block list --url "http://$api:8008" \
+        | cut -f 1 -d " "); \
+      echo $api && echo $BLOCK_LIST;
+      if [[ $BLOCK_LIST == *"30"* ]]; then \
+        echo "API $api is on block 30" && ((NODES_ON_30++)); \
+      else \
+        echo "API $api is not yet on block 30"; \
+      fi; \
+    done; \
+  done;'
+echo "All nodes have reached block 30!"
+
+echo "Dumping logs"
+echo "-- Workload --"
+docker-compose -p ${ISOLATION_ID} -f /project/sawtooth-pbft/adhoc/workload.yaml logs
+echo "-- Alpha --"
+docker-compose -p ${ISOLATION_ID}-alpha -f /project/sawtooth-pbft/adhoc/node.yaml logs
+echo "-- Beta --"
+docker-compose -p ${ISOLATION_ID}-beta -f /project/sawtooth-pbft/adhoc/node.yaml logs
+echo "-- Gamma --"
+docker-compose -p ${ISOLATION_ID}-gamma -f /project/sawtooth-pbft/adhoc/node.yaml logs
+echo "-- Gamma --"
+GENESIS=1 docker-compose -p ${ISOLATION_ID}-epsilon -f /project/sawtooth-pbft/adhoc/node.yaml logs
+echo "-- Admin --"
+docker-compose -p ${ISOLATION_ID} -f /project/sawtooth-pbft/adhoc/admin.yaml logs
+
+echo "Done testing; shutting down all containers"
+docker-compose -p ${ISOLATION_ID} -f /project/sawtooth-pbft/adhoc/workload.yaml down && \
+docker-compose -p ${ISOLATION_ID}-alpha -f /project/sawtooth-pbft/adhoc/node.yaml down && \
+docker-compose -p ${ISOLATION_ID}-beta -f /project/sawtooth-pbft/adhoc/node.yaml down && \
+docker-compose -p ${ISOLATION_ID}-gamma -f /project/sawtooth-pbft/adhoc/node.yaml down && \
+GENESIS=1 docker-compose -p ${ISOLATION_ID}-epsilon -f /project/sawtooth-pbft/adhoc/node.yaml down && \
+docker-compose -p ${ISOLATION_ID} -f /project/sawtooth-pbft/adhoc/admin.yaml down
