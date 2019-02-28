4da7894b2f6f5113686802de52308c232afbc5c2
Logan Seeley <seeley@bitwise.io>
2019-02-28 10:17:37 -0600

    Add new message log unit tests
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- src/message_log.rs
+++ src/message_log.rs
@@ -202,7 +202,161 @@ mod tests {
     use super::*;
     use crate::config;
     use crate::protos::pbft_message::PbftMessage;
+    use crate::test_helpers::*;
     use sawtooth_sdk::consensus::engine::PeerId;
+
+    /// The `PbftLog` must reliably store and retrieve blocks for the node to keep track of the
+    /// blocks it receives from the validator, perform consensus on them, and commit or fail them.
+    ///
+    /// All blocks are added to the `PbftLog` using the `add_block` method, and they are retrieved
+    /// using the following methods:
+    /// - `get_block_with_id` will get the block that matches the specified block ID
+    /// - `get_blocks_with_num` will get all blocks that have the specified block number
+    ///
+    /// This test will verify that blocks can be added to a `PbftLog` using its `add_block` method
+    /// and that the methods for retrieving blocks work as intended.
+    #[test]
+    fn test_block_logging() {
+        // Initialize an empty log
+        let cfg = mock_config(4);
+        let mut log = PbftLog::new(&cfg);
+
+        // Add block 1 to the log
+        let block1 = mock_block(1);
+        log.add_block(block1.clone());
+
+        // Verify log correctly retrieves blocks by ID
+        assert_eq!(
+            &block1,
+            log.get_block_with_id(&block1.block_id)
+                .expect("Block 1 not retrieved"),
+        );
+        assert!(log.get_block_with_id(&vec![2]).is_none());
+
+        // Add more blocks
+        let mut block2 = mock_block(2);
+        block2.block_num = 1;
+        log.add_block(block2.clone());
+        let block3 = mock_block(3);
+        log.add_block(block3.clone());
+
+        // Verify log correctly retrieves blocks by number
+        let blocks_with_num_1 = log.get_blocks_with_num(1);
+        assert_eq!(2, blocks_with_num_1.len());
+        assert!(blocks_with_num_1.contains(&&block1));
+        assert!(blocks_with_num_1.contains(&&block2));
+    }
+
+    /// The log must reliably store PBFT messages so that each node can use these messages to
+    /// verify the progress of the network as it performs consensus on various blocks and decides
+    /// on view changes.
+    ///
+    /// However, to ensure that messages are counted properly and that duplicate messages/blocks
+    /// can’t be used to by a malicious node to trick the network, the `PbftLog` must not allow
+    /// duplicate messages. If duplicate messages were not prevented, a bad node could, for
+    /// instance, send the same `Commit` message multiple times to trick the other nodes into
+    /// thinking that the necessary number of Commit messages have been received to safely commit
+    /// that block.
+    ///
+    /// All messages are added to the `PbftLog` using the `add_message` method, and messages are
+    /// retrieved using these methods that each provide a different filter for the messages:
+    /// - `get_messages_of_type_seq` will retrieve all messages that match the specified message
+    ///   type and sequence number
+    /// - `get_messages_of_type_view` will retrieve all message that match the specified message
+    ///   type and view number
+    /// - `get_messages_of_type_seq_view` will retrieve all message that match the specified
+    ///   message type, sequence number, and view number
+    /// - `get_messages_of_type_seq_view_block` will retrieve all messages that match the specified
+    ///   message type, sequence number, view number, and block ID
+    ///
+    /// This test will verify that messages can be added to a `PbftLog` using its `add_message`
+    /// method, that the log will not store duplicate messages, and that the methods for retrieving
+    /// messages work as intended.
+    #[test]
+    fn test_message_logging() {
+        // Initialize an empty log
+        let cfg = mock_config(4);
+        let mut log = PbftLog::new(&cfg);
+
+        // Verify adding single message works
+        let msg1 = mock_msg(PbftMessageType::PrePrepare, 0, 1, vec![0], vec![1]);
+        log.add_message(msg1.clone());
+        assert_eq!(1, log.messages.len());
+        assert!(log.messages.contains(&msg1));
+
+        // Verify messages aren't duplicated
+        log.add_message(msg1.clone());
+        assert_eq!(1, log.messages.len());
+
+        // Verify get_messages_of_type_seq() works
+        let msg2 = mock_msg(PbftMessageType::Commit, 0, 1, vec![0], vec![1]);
+        log.add_message(msg2.clone());
+        let msg3 = mock_msg(PbftMessageType::Commit, 0, 2, vec![0], vec![2]);
+        log.add_message(msg3.clone());
+        let msg4 = mock_msg(PbftMessageType::Commit, 1, 2, vec![0], vec![2]);
+        log.add_message(msg4.clone());
+
+        let res1 = log.get_messages_of_type_seq(PbftMessageType::Commit, 1);
+        assert_eq!(1, res1.len());
+        assert!(res1.contains(&&msg2));
+
+        let res2 = log.get_messages_of_type_seq(PbftMessageType::Commit, 2);
+        assert_eq!(2, res2.len());
+        assert!(res2.contains(&&msg3));
+        assert!(res2.contains(&&msg4));
+
+        // Verify get_messages_of_type_view() works
+        let msg5 = mock_msg(PbftMessageType::ViewChange, 0, 1, vec![0], vec![1]);
+        log.add_message(msg5.clone());
+        let msg6 = mock_msg(PbftMessageType::ViewChange, 1, 1, vec![0], vec![1]);
+        log.add_message(msg6.clone());
+        let msg7 = mock_msg(PbftMessageType::ViewChange, 1, 2, vec![0], vec![2]);
+        log.add_message(msg7.clone());
+
+        let res3 = log.get_messages_of_type_view(PbftMessageType::ViewChange, 0);
+        assert_eq!(1, res3.len());
+        assert!(res3.contains(&&msg5));
+
+        let res4 = log.get_messages_of_type_view(PbftMessageType::ViewChange, 1);
+        assert_eq!(2, res4.len());
+        assert!(res4.contains(&&msg6));
+        assert!(res4.contains(&&msg7));
+
+        // Verify get_messages_of_type_seq_view() works
+        let msg8 = mock_msg(PbftMessageType::Commit, 0, 1, vec![1], vec![1]);
+        log.add_message(msg8.clone());
+
+        let res5 = log.get_messages_of_type_seq_view(PbftMessageType::Commit, 1, 0);
+        assert_eq!(2, res5.len());
+        assert!(res5.contains(&&msg2));
+        assert!(res5.contains(&&msg8));
+
+        let res6 = log.get_messages_of_type_seq_view(PbftMessageType::Commit, 2, 0);
+        assert_eq!(1, res6.len());
+        assert!(res6.contains(&&msg3));
+
+        let res7 = log.get_messages_of_type_seq_view(PbftMessageType::Commit, 2, 1);
+        assert_eq!(1, res7.len());
+        assert!(res7.contains(&&msg4));
+
+        let res8 = log.get_messages_of_type_seq_view(PbftMessageType::PrePrepare, 1, 0);
+        assert_eq!(1, res8.len());
+        assert!(res8.contains(&&msg1));
+
+        // Verify get_messages_of_type_seq_view_block() works
+        let msg9 = mock_msg(PbftMessageType::Commit, 0, 1, vec![0], vec![2]);
+        log.add_message(msg9.clone());
+
+        let res9 = log.get_messages_of_type_seq_view_block(PbftMessageType::Commit, 1, 0, &vec![1]);
+        assert_eq!(2, res9.len());
+        assert!(res9.contains(&&msg2));
+        assert!(res9.contains(&&msg8));
+
+        let res10 =
+            log.get_messages_of_type_seq_view_block(PbftMessageType::Commit, 1, 0, &vec![2]);
+        assert_eq!(1, res10.len());
+        assert!(res10.contains(&&msg9));
+    }
 
     /// Create a PbftMessage, given its type, view, sequence number, and who it's from
     fn make_msg(
